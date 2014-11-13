77b8687bafe7559892bc5943637d05a9be246354
Lukas Zapletal <lzap+git@redhat.com>
2014-11-13 10:38:33 +0100

    Adding Vagrant-Rackspace support for Jenkins

---
+++ aux/vagrant-build/build_image.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# vim: sw=2:ts=2:et
+set -x
+export repoowner=${1:-theforeman}
+export branch=${2:-master}
+export proxy_repo=${3:-http://yum.theforeman.org/nightly/el6/x86_64/}
+NAME=foreman-discovery-image
+
+# give the VM some time to finish booting and network configuration
+sleep 30
+yum -y install livecd-tools appliance-tools-minimizer fedora-packager \
+  python-devel rpm-build createrepo selinux-policy-doc checkpolicy \
+  selinux-policy-devel autoconf automake python-mock python-lockfile \
+  python-nose git-review qemu-kvm hardlink git wget
+
+# build plugin
+pushd /root
+SELINUXMODE=$(getenforce)
+setenforce 1
+
+[ -d $NAME ] || git clone --depth 1 https://github.com/$repoowner/$NAME.git -b $branch
+pushd $NAME
+git pull
+
+./build-livecd fdi-centos7.ks && sudo ./build-livecd-root
+ls fdi-image*tar -lah
+
+popd
+setenforce $SELINUXMODE
