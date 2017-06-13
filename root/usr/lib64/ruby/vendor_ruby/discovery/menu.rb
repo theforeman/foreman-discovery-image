59e05b777c5d3ef62fdbcf502a7ab51c03e2f20f
Lukáš Zapletal <lzap@redhat.com>
2017-06-13 20:30:47 +0200

    Fixes #19984 - added fdi.countdown to unattended pxeless

--- root/usr/lib64/ruby/vendor_ruby/discovery/menu.rb
+++ root/usr/lib64/ruby/vendor_ruby/discovery/menu.rb
@@ -173,6 +173,9 @@ def main_loop
           status = configure_network false, mac
         end
         log_debug "Unattended network configuration finished, result: #{status}"
+        delay = cmdline('fdi.countdown', 10)
+        log_debug "Delay for network initialization: #{delay} seconds"
+        sleep delay
         facts = new_custom_facts(mac)
         log_debug "Unattended facts upload started"
         result = upload(proxy_url, proxy_type, facts)
