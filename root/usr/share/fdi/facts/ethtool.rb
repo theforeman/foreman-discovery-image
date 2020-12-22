09c7b7a4a1c8268427c109ad61a13e3abe71213c
Patrick Kuijvenhoven <pk@worldstream.nl>
2020-12-22 22:43:49 +0100

    Reintroduce ethtool facts
    ethtool facts were removed in 68f2446aa90c5436e0ca85925fd9b6ea222fdb8e

---
+++ root/usr/share/fdi/facts/ethtool.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+#
+# Quickfix for regression introduced in FDI 3.7.0
+# See: https://community.theforeman.org/t/foreman-discovery-image-3-7/21642
+#
+# To test:
+# $ FACTERLIB=/usr/share/fdi/facts tfm-ruby /opt/theforeman/tfm/root/usr/bin/facter
+
+require 'facter'
+
+Facter.add(:ethtool, :timeout => 10) do
+  confine :kernel => "Linux"
+
+  confine do
+    Facter::Core::Execution.which('ethtool') != nil
+  end
+
+  setcode do
+    interface_info = {}
+    Facter.value(:networking)['interfaces'].keys.each do |interface|
+      Facter.debug("Running ethtool on interface #{interface}")
+      output = Facter::Core::Execution.exec("ethtool #{interface} 2>/dev/null")
+      if output.nil?
+        Facter.debug("Execution of ethtool on interface #{interface} failed")
+      else
+        attributes = {}
+        output.each_line do |line|
+          next if line.nil? or line == ""
+          case line.strip
+          when /Speed: (.*)Mb/
+            attributes[:speed] = $1
+          when /Duplex: (.*)/
+            attributes[:duplex] = $1.downcase
+          when /Port: (.*)/
+            attributes[:port] = $1
+          when /Auto-negotiation: (.*)/
+            attributes[:auto_negotiation] = ($1 == 'on').to_s
+          when /^Wake-on: (.*)/
+            attributes[:wol] = $1.include?('g')
+          when /Link detected: (.*)/
+            attributes[:link] = ($1 == 'yes').to_s
+          end
+        end
+
+        if attributes.keys.empty?
+          Facter.debug("Running ethtool on #{interface} didn't give any information")
+        else
+          interface_info[interface] = attributes
+        end
+      end
+    end
+    interface_info
+  end
+end
