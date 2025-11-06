c0a7900317d3a489b4e17e48257dbf94600778d9
Evgeni Golov <evgeni@golov.de>
2025-11-06 09:12:50 +0100

    explicitly cast ip/gw/dns to String
    We might have been called with `nil`, but Newt doesn't like that.

--- root/usr/share/ruby/vendor_ruby/discovery/screen/network.rb
+++ root/usr/share/ruby/vendor_ruby/discovery/screen/network.rb
@@ -6,9 +6,9 @@ def screen_network mac, vlan_id, ip = cmdline('fdi.pxip', ''), gw = cmdline('fdi
   l_ip = Newt::Label.new(2, 8, _("Address:"))
   l_gw = Newt::Label.new(2, 10, _("Gateway:"))
   l_dns = Newt::Label.new(2, 12, _("DNS:"))
-  t_ip = Newt::Entry.new(16, 8, ip, 55, Newt::FLAG_SCROLL)
-  t_gw = Newt::Entry.new(16, 10, gw, 55, Newt::FLAG_SCROLL)
-  t_dns = Newt::Entry.new(16, 12, dns, 55, Newt::FLAG_SCROLL)
+  t_ip = Newt::Entry.new(16, 8, ip.to_s, 55, Newt::FLAG_SCROLL)
+  t_gw = Newt::Entry.new(16, 10, gw.to_s, 55, Newt::FLAG_SCROLL)
+  t_dns = Newt::Entry.new(16, 12, dns.to_s, 55, Newt::FLAG_SCROLL)
   b_ok = Newt::Button.new(24, 15, _("Next"))
   b_cancel = Newt::Button.new(36, 15, _("Cancel"))
   items = [t_desc, l_ip, l_gw, l_dns, t_ip, t_gw, t_dns]
